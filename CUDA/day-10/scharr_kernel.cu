3baf5b45f138ef6f724bd5eac48cd97e90fbc249
Diwakar Basnet <basnet7diwakar@gmail.com>
2025-03-03 12:56:56 +0545

    updated scharr kernel with cuda forward function

--- CUDA/day-10/scharr_kernel.cu
+++ CUDA/day-10/scharr_kernel.cu
@@ -36,8 +36,68 @@ __global__ void scharr_kernel(
 
         for (int f_row = 0; f_row < FILTER_RADIUS; f_row++) {
             for (int f_col = 0; f_col < FILTER_RADIUS; f_col++) {
-                
+                if (threadIdx.x-FILTER_RADIUS+fCol >= 0 &&
+                    threadIdx.x-FILTER_RADIUS+fCol < TILE_DIM &&
+                    threadIdx.y-FILTER_RADIUS+fRow >= 0 &&
+                    threadIdx.y-FILTER_RADIUS+fRow < TILE_DIM) {
+                        sum_x += SCHARR_X[fRow][fCol] * input_tile[threadIdx.y - FILTER_RADIUS + fRow][threadIdx.x - FILTER_RADIUS + fCol];
+                        sum_y += SCHARR_Y[fRow][fCol] * input_tile[threadIdx.y - FILTER_RADIUS + fRow][threadIdx.x - FILTER_RADIUS + fCol];
+                    }
+                else {
+                    if (row - FILTER_RADIUS + fRow >= 0 &&
+                        row - FILTER_RADIUS + fRow < height &&
+                        col - FILTER_RADIUS + fCol >= 0 &&
+                        col - FILTER_RADIUS + fCol < width) {
+                            sum_x += SCHARR_X[fRow][fCol] * image[(row - FILTER_RADIUS + fRow) * width + (col - FILTER_RADIUS + fCol)];
+                            sum_y += SCHARR_Y[fRow][fCol] * image[(row - FILTER_RADIUS + fRow) * width + (col - FILTER_RADIUS + fCol)];
+                        }
+                }
             }
         }
+        output[row * width + col] = sqrtf(sum_x * sum_x + sum_y * sum_y);
     }
+}
+
+void scharr_filters() {
+    float scharr_x[FILTER_SIZE][FILTER_SIZE] = {
+        {-3, 0, 3},
+        {-10, 0, 10},
+        {-3, 0, 3}
+    };
+    float scharr_y[FILTER_SIZE][FILTER_SIZE] = {
+        {-3, -10, -3},
+        {0, 0, 0},
+        {3, 10, 3}
+    };
+
+    cudaMemcpyToSymbol(SCHARR_X, scharr_x, FILTER_SIZE * FILTER_SIZE * sizeof(float));
+    cudaMemcpyToSymbol(SCHARR_Y, scharr_y, FILTER_SIZE * FILTER_SIZE * sizeof(float));
+}
+
+torch::Tensor scharr_cuda_forward(torch::Tensor input) {
+    input = input.contiguous();
+
+    const int height = input.size(0);
+    const int width = input.size(1);
+
+    auto output = torch::zeros_like(input);
+
+    const dim3 dimBlock(TILE_DIM, TILE_DIM, 1);
+    const dim3 dimGrid((width + TILE_DIM -1)/TILE_DIM, (height + TILE_DIM - 1)/TILE_DIM, 1);
+
+    scharr_filters();
+
+    scharr_kernel<<<dimGrid, dimBlock>>>(
+        input.data_ptr<float>(),
+        output.data_ptr<float>(),
+        width,
+        height
+    );
+
+    cudaError_t err = cudaDeviceSynchronize();
+    if (err != cudaSuccess) {
+        printf("Error in scharr_cuda_forward: %s\n", cudaGetErrorString(err));
+    }
+
+    return output;
 }
