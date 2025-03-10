e91992216c171da4e19947a2fc976644778f8e3b
Diwakar Basnet <basnet7diwakar@gmail.com>
2025-03-10 07:50:12 +0545

    added layer normalization kernel

---
+++ CUDA/day-12/layer_normalization.cu
@@ -0,0 +1,36 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+#define gamma 1.0f
+#define beta 0.0f
+
+__global__ void layer_normalization_kernel(
+    float *A, float *B, float *mean, float *variance, int width, int height
+) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < height) {
+        float sum = 0.0f;
+        for (int col =0; col < width; col++) {
+            sum += A[row * width + col];
+        }
+        mean[row] = sum / width;
+        sum = 0.0f;
+        for (int col = 0; col < width; col++) {
+            sum += (A[row * width + col] - mean[row]) * (A[row * width + col] - mean[row]);
+        }
+        variance[row] = sum / width;
+        for (int col = 0; col < width; col++) {
+            B[row * width + col] = gamma * ((A[row * width + col] - mean[row]) / sqrtf(variance[row] + 1e-6)) + beta;
+        }
+    }
+}
+
+void cudaLayerNorm(
+    float *input, float *output, float *mean, float *variance, int width, int height
+) {
+    const dim3 blockSize(256);
+    const dim3 gridSize((height + blockSize.x - 1) / blockSize.x);
+
+    layer_normalization_kernel<<<gridSize, blockSize>>>(input, output, mean, variance, width, height);
+    cudaDeviceSynchronize();
+}
