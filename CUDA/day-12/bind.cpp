c782e39009cc6c2100b854a6b4d75a4b55a43746
Diwakar Basnet <basnet7diwakar@gmail.com>
2025-03-12 08:01:58 +0545

    updated code for 3d tensor

--- CUDA/day-12/bind.cpp
+++ CUDA/day-12/bind.cpp
@@ -1,16 +1,27 @@
 #include <torch/extension.h>
 
-void cudaLayerNormalization(float *input, float *output, float *mean, float *variance, int width, int height);
+void cudaLayerNormalization(
+    float *input, 
+    float *output, 
+    int batch_size, 
+    int seq_len, 
+    int embed_dim
+);
 torch::Tensor layerNorm(torch::Tensor input) {
+    const int batch_size = input.size(0);
+    const int seq_len = input.size(1);
+    const int embed_dim = input.size(2);
+
     torch::Tensor output = torch::zeros_like(input);
-    const int height = input.size(0);
-    const int width = input.size(1);
+    
     cudaLayerNorm(
         input.data_ptr<float>(),
         output.data_ptr<float>(),
-        height,
-        width
+        batch_size,
+        seq_len,
+        embed_dim
     );
+    
     return output;
 }
 
